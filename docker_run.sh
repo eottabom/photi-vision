b7a823b35081504c3ff00be1a6f2f259a2f1428e
deep-learning-server <jm84.joo@gmail.com>
2019-12-07 01:21:26 +0900

    Update Makefile and docker code

--- docker_run.sh
+++ docker_run.sh
@@ -1,2 +1,2 @@
 # Test from Docker version 18.06.2-ce, build 6d37f41
-docker run --runtime=nvidia -it photi-vision/parking-lot-detection:0.0.1
+docker run --runtime=nvidia -it -v ${HOME}/experiments/photi-vision-data:/workspace/photi-vision-data photi-vision/parking-lot-detection:0.0.1
