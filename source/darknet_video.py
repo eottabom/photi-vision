6cd63560989af55d5fae2fb3b5c57c6cb37d36bf
deep-learning-server <jm84.joo@gmail.com>
2019-12-18 02:06:45 +0900

    Update logic

--- source/darknet_video.py
+++ source/darknet_video.py
@@ -8,6 +8,19 @@ import time
 import darknet
 import matplotlib.pyplot as plt
 
+def bb_center_dist(bb1, bb2):
+    x1 = bb1[0]
+    y1 = bb1[1]
+    x2 = bb2[0]
+    y2 = bb2[1]
+    
+    a = x1 - x2
+    b = y1 - y2
+    
+    dist = math.sqrt((a * a) + (b * b))
+    
+    return dist
+    
 def bb_intersection_over_union(boxA, boxB):
     xA = max(boxA[0], boxB[0])
     yA = max(boxA[1], boxB[1])
@@ -38,7 +51,7 @@ def convertBack(x, y, w, h):
     ymin = int(round(y - (h / 2)))
     ymax = int(round(y + (h / 2)))
     return xmin, ymin, xmax, ymax
-
+'''
 def cvDrawBoxes(detections, img):
     #     print('detections length : ', len(detections))
     pt1 = (0, 0)
@@ -61,15 +74,19 @@ def cvDrawBoxes(detections, img):
         xmin, ymin, xmax, ymax = convertBack( float(x), float(y), float(w), float(h))
         pt1 = (xmin, ymin)
         pt2 = (xmax, ymax)
-        cv2.rectangle(img, pt1, pt2, (0, 255, 0), 3)
-        #         cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), -1)
-        cv2.putText(img,
-                    detection[0].decode() +
-                    " [" + str(round(detection[1] * 100, 2)) + "]",
-                    (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
-                    [0, 255, 0], 2)
         area = (ymax-ymin) * (xmax-xmin)
+        #print('area:', area)
+        if area >= 13000:
+            cv2.rectangle(img, pt1, pt2, (0, 255, 0), 3)
+        #         cv2.rectangle(img, (xmin, ymin), (xmax, ymax), (0, 255, 0), -1)
+            cv2.putText(img,
+                        detection[0].decode() +
+                        " [" + str(round(detection[1] * 100, 2)) + "]",
+                        (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        [0, 255, 0], 2)
+        #area = (ymax-ymin) * (xmax-xmin)
     return img, area, (xmin, ymin, xmax, ymax)
+'''
 
 outputWidth = 1920
 outputHeight = 1080
@@ -117,11 +134,11 @@ def YOLO():
         except Exception:
             pass
     #cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture("./photi-vision-data/my_parking_lot_02.mp4")
+    cap = cv2.VideoCapture("./photi-vision-data/parking04_edit.mp4")
     cap.set(3, outputWidth)
     cap.set(4, outputHeight)
     out = cv2.VideoWriter(
-        "./photi-vision-data/output-test-3.mp4", cv2.VideoWriter_fourcc(*"MP4V"), 30.0,
+        "./photi-vision-data/output-parking04_edit.mp4", cv2.VideoWriter_fourcc(*"MP4V"), 30.0,
         (outputWidth, outputHeight))
     print("Starting the YOLO loop...")
 
@@ -139,10 +156,12 @@ def YOLO():
 
     #temp_images = []
     temp_rectangles = []
+    temp_centers = []
 
     free_space_frames = 0
 
     while True:
+        temp_centers.clear()
         temp_rectangles.clear()
         prev_time = time.time()
         ret, frame_read = cap.read()
@@ -159,8 +178,18 @@ def YOLO():
         darknet.copy_image_from_bytes(darknet_image,frame_resized.tobytes())
 
         detections = darknet.detect_image(netMain, metaMain, darknet_image, thresh=0.25)
-        image, area, rectangleB = cvDrawBoxes(detections, frame_resized)
+        #image, area, rectangleB = cvDrawBoxes(detections, frame_resized)
+        image = frame_resized
 
+        pt1 = (0, 0)
+        pt2 = (0, 0)
+        area = 0
+    
+        xmin = 0
+        ymin = 0
+        xmax = 0
+        ymax = 0
+        
         for detection in detections:
             if detection[0].decode() != 'car': # and detection[0].decode() != 'bus' and detection[0].decode() != 'truck':
                 continue
@@ -170,8 +199,19 @@ def YOLO():
                 detection[2][2],\
                 detection[2][3]
             xmin, ymin, xmax, ymax = convertBack( float(x), float(y), float(w), float(h))
-
-            temp_rectangles.append((xmin, ymin, xmax, ymax))
+            area = (ymax-ymin) * (xmax-xmin)
+            pt1 = (xmin, ymin)
+            pt2 = (xmax, ymax)
+            
+            if area >= 12000:
+                cv2.rectangle(image, pt1, pt2, (0, 255, 0), 3)
+                cv2.putText(image,
+                        detection[0].decode() +
+                        " [" + str(round(detection[1] * 100, 2)) + "]",
+                        (pt1[0], pt1[1] - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                        [0, 255, 0], 2)
+                temp_rectangles.append((xmin, ymin, xmax, ymax))
+                temp_centers.append( (xmin + (xmax-xmin)/2, ymin + (ymax-ymin)/2) )
 
         num_of_bb = len(temp_rectangles)
         print("number of bounding box in current scene:"+str(len(temp_rectangles)))
@@ -182,12 +222,19 @@ def YOLO():
             for j in range(0, len(temp_rectangles)):
                 try:
                     iou, center, start, end = bb_intersection_over_union(temp_rectangles[i], temp_rectangles[j])
+                    dist = bb_center_dist(temp_centers[i], temp_centers[j])
+                    #area_i = bb_area(temp_rectangles[i])
+                    #area_j = bb_area(temp_rectangles[j])
+                    #print('dist:', dist)
                     #print('::::', iou, '::::', center, '::::::', temp_rectangles[i] , '::::::', temp_rectangles[j])
                 except ZeroDivisionError:
                     print("ZeroDivision")
-                if iou <= 0.05 and iou > 0:
+                if iou <= 0.05 and iou > 0 and dist <= 320:
                     free_space = True
-                    cv2.rectangle(image, start, end, (0,0,255), -1)
+                    print("Free space exits!")
+                    cv2.rectangle(image, start, end, (255,0,0), -1)
+                    #cv2.line(image, (temp_rectangles[i][0], temp_rectangles[i][3]), (temp_rectangles[j][0], temp_rectangles[j][3]), (255,0,0), 2)
+                    #cv2.line(image, (temp_rectangles[i][2], temp_rectangles[i][3]), (temp_rectangles[j][2], temp_rectangles[j][3]), (255,0,0), 2)
 
         if free_space:
             free_space_frames += 1
